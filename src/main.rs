5a34e900379d46daf517fb02750b9824c5641999
Michael Sperone <mike@mikesperone.com>
2019-04-25 12:14:01 -0500

    ux improvements

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ use rosc::encoder;
 
 // For CLI
 extern crate term;
-use std::io::prelude::*;
+// use std::io::prelude::*;
 
 // #[derive(Eq, Copy, Clone)]
 // struct HeldKeys {
@@ -59,9 +59,20 @@ fn main() {
 //     }
 // }
 
+fn clear_screen() {
+    let mut t = term::stdout().unwrap();
+    t.fg(term::color::WHITE).unwrap();
+    t.reset().unwrap();
+    print!("{}[2J", 27 as char);
+    print!("{}[0;0H", 27 as char);
+}
+
 fn run() -> Result<(), Box<Error>> {
 
     let mut t = term::stdout().unwrap();
+    clear_screen();
+    stdout().flush().unwrap();
+
     // ------------------------------
     //  1. Get MIDI IO
     // ------------------------------
@@ -71,6 +82,7 @@ fn run() -> Result<(), Box<Error>> {
     midi_in.ignore(Ignore::None);
     let midi_out = MidiOutput::new("midir forwarding output")?;
 
+    // Input ports
     println!("Available input ports:");
     for i in 0..midi_in.port_count() {
         println!("{}: {}", i, midi_in.port_name(i)?);
@@ -79,29 +91,47 @@ fn run() -> Result<(), Box<Error>> {
     stdout().flush()?;
     stdin().read_line(&mut input)?;
     let in_port: usize = input.trim().parse()?;
+    let in_port_name = midi_in.port_name(in_port)?;
 
-    println!("\nAvailable output ports:");
-    for i in 0..midi_out.port_count() {
-        println!("{}: {}", i, midi_out.port_name(i)?);
-    }
-    println!("Please select output port:");
+    // Output
+    print!("Would you like to forward MIDI to another destination? [Y|y]");
     stdout().flush()?;
     input.clear();
     stdin().read_line(&mut input)?;
-    let out_port: usize = input.trim().parse()?;
+    let ans: char = input.trim().parse()?;
+    let mut is_forwarding_midi: bool = false;
+    let mut out_port: usize = 0;
+    let mut out_port_name = String::from("default");
+    let positive_response: &str = "Yy";
+    if positive_response.contains(ans) {
 
-    let in_port_name = midi_in.port_name(in_port)?;
-    let out_port_name = midi_out.port_name(out_port)?;
+        is_forwarding_midi = true;
+        println!("\nAvailable output ports:");
+        for i in 0..midi_out.port_count() {
+            println!("{}: {}", i, midi_out.port_name(i)?);
+        }
+        println!("Please select output port:");
+        stdout().flush()?;
+        input.clear();
+        stdin().read_line(&mut input)?;
+        out_port = input.trim().parse()?;
+        out_port_name = midi_out.port_name(out_port)?;
 
-    println!("\n------------------------------------------------");
-    println!("\n  Reading MIDI in on port {:?}", in_port_name);
-    println!("\n  Sending MIDI out on port {:?}", out_port_name);
-    println!("\n------------------------------------------------");
+    }
+
+    clear_screen();
+    t.fg(term::color::BLUE).unwrap();
+    writeln!(t, "\n------------------------------------------------").unwrap();
+    writeln!(t, "\n  Reading MIDI in on port {:?}", in_port_name).unwrap();
+    if is_forwarding_midi {
+        writeln!(t, "\n  Sending MIDI out on port {:?}", out_port_name).unwrap();
+    }
+    writeln!(t, "\n------------------------------------------------").unwrap();
+    t.reset().unwrap();
 
     // ------------------------------
     //  2. Forward MIDI
     // ------------------------------
-    let mut conn_out = midi_out.connect(out_port, "midir-forward")?;
 
     let mut pc_bucket = Vec::new();
     let from_address = "127.0.0.1:57000";
@@ -110,8 +140,13 @@ fn run() -> Result<(), Box<Error>> {
     let destination_host_name = SocketAddrV4::from_str(&to_address).unwrap();
     let sock = UdpSocket::bind(my_host_name).unwrap();
 
+    let mut conn_out = midi_out.connect(out_port, "midir-forward")?;
     let _conn_in = midi_in.connect(in_port, "midir-forward", move |_stamp, message, _| {
-        conn_out.send(message).unwrap_or_else(|_| println!("Error when forwarding message ..."));
+        if is_forwarding_midi {
+            conn_out
+                .send(message)
+                .unwrap_or_else(|_| println!("Error when forwarding message ..."));
+        }
         let pitch = message[1];
         let velocity = message[2];
         let pitch_class = pitch % 12;
@@ -137,15 +172,23 @@ fn run() -> Result<(), Box<Error>> {
 
         t.cursor_up().unwrap();
         t.delete_line().unwrap();
+        t.fg(term::color::GREEN).unwrap();
         writeln!(t, "{:?}", &pc_bucket).unwrap();
         // println!("pitch: {}, (pc: {}), velocity: {}", pitch, pitch_class, velocity);
     }, ())?;
 
-    println!("Connections open, forwarding from '{}' to '{}' (press enter to exit) ...", in_port_name, out_port_name);
+    print!("Connections open ");
+    if is_forwarding_midi {
+        print!("forwarding from '{}' to '{}' ", in_port_name, out_port_name);
+    }
+    print!("(press enter to exit) ...\n\r");
+    println!("\n\r");
 
     input.clear();
     stdin().read_line(&mut input)?;
 
-    println!("Closing connections");
+    clear_screen();
+    println!("Closing connections...");
+    println!("Goodbye.");
     Ok(())
 }
